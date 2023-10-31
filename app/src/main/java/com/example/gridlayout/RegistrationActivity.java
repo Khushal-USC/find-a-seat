bdbe0105745b9954aeb80d0bded4f02b5065108f
JulianGlover4 <julianglover4@gmail.com>
2023-10-31 03:03:21 -0700

    Julian edits

--- app/src/main/java/com/example/gridlayout/RegistrationActivity.java
+++ app/src/main/java/com/example/gridlayout/RegistrationActivity.java
@@ -1,16 +1,24 @@
 package com.example.gridlayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -19,6 +27,19 @@ public class RegistrationActivity extends AppCompatActivity {
 
     Spinner spinner;
     String[] items = {"Select:", "Undergraduate", "Graduate", "Faculty", "Staff"};
+
+    private EditText emailInput;
+    private EditText passwordInput;
+    private EditText nameInput;
+    private EditText idInput;
+
+    private Button submission;
+
+    private TextView errorEmail;
+    private TextView errorPassword;
+    private TextView errorName;
+    private TextView errorID;
+    private TextView errorAffiliation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +56,24 @@ public class RegistrationActivity extends AppCompatActivity {
 
         spinner.setAdapter(adapter);
 
+        emailInput = findViewById(R.id.editTextText);
+        passwordInput = findViewById(R.id.editTextText4);
+        nameInput = findViewById(R.id.editTextText5);
+        idInput = findViewById(R.id.editTextText6);
+
+        submission = findViewById(R.id.button2);
+
+        errorEmail = findViewById(R.id.textView19);
+        errorPassword = findViewById(R.id.textView20);
+        errorName = findViewById(R.id.textView21);
+        errorID = findViewById(R.id.textView22);
+        errorAffiliation = findViewById(R.id.textView23);
+
+        errorEmail.setTextColor(Color.WHITE);
+        errorPassword.setTextColor(Color.WHITE);
+        errorName.setTextColor(Color.WHITE);
+        errorID.setTextColor(Color.WHITE);
+
         buttonGallery.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
 
@@ -44,6 +83,75 @@ public class RegistrationActivity extends AppCompatActivity {
 
             }
         });
+
+        submission.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String grabEmail = emailInput.getText().toString();
+                String grabPassword = passwordInput.getText().toString();
+                String grabName = nameInput.getText().toString();
+                String grabID = idInput.getText().toString();
+
+                if(!checkPasswordGood(grabPassword)){
+                    errorPassword.setText("Invalid Password Input");
+                    errorPassword.setTextColor(Color.RED);
+                }else{
+                    errorPassword.setTextColor(Color.WHITE);
+                }
+
+                if(grabID.length() == 10 && grabID.matches("\\d+")){
+                    errorID.setTextColor(Color.WHITE);
+                }else{
+                    errorID.setText("Invalid ID");
+                    errorID.setTextColor(Color.RED);
+                }
+
+                if(nameInput.length() == 0){
+                    errorName.setText("Invalid Input");
+                    errorName.setTextColor(Color.RED);
+                }else{
+                    errorName.setTextColor(Color.WHITE);
+                }
+
+                if(grabEmail.endsWith("@usc.edu")){
+                    errorEmail.setTextColor(Color.WHITE);
+                }else{
+                    errorEmail.setText("Invalid Email");
+                    errorEmail.setTextColor(Color.RED);
+                }
+
+                if((String) spinner.getSelectedItem() == "Select:"){
+                    errorAffiliation.setText("Please Select An Affiliation");
+                    errorAffiliation.setTextColor(Color.RED);
+                }else{
+                    errorAffiliation.setTextColor(Color.WHITE);
+                }
+
+            }
+        });
+
+
+    }
+
+    boolean checkPasswordGood(String s){
+
+        if(s.length() < 8){
+            return false;
+        }
+
+        String regex = "[^a-zA-Z0-9]";
+
+        // Create a Pattern object
+        Pattern pattern = Pattern.compile(regex);
+
+        // Create a Matcher object
+        Matcher matcher = pattern.matcher(s);
+
+        if (matcher.find()) {
+            return true;
+        } else {
+            return false;
+        }
 
     }
 
